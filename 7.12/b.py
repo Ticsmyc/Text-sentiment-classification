e745e1ac10a37b760e18048ccc367a9c1b5f6939
Island-c <liu0148@outlook.com>
2018-07-12 19:29:18 +0800

    支持向量机（不加词向量模型) 准确率79%

--- 7.12/b.py
+++ 7.12/b.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from gensim.models import word2vec
 import jieba
 from sklearn.svm import SVC
-import gensim
+
 
 def cut_texts(texts=None, word_len=1, savename=None):
     #分词
@@ -32,13 +32,25 @@ def cut_texts(texts=None, word_len=1, savename=None):
     return texts_cut
 
 
-def text2vec(texts_cut=None, word2vec_loadpath=None,merge=False,needSave=False):
-
-     model=gensim.models.Word2Vec.load(word2vec_loadpath)
-#     model = gensim.models.KeyedVectors.load_word2vec_format('sgns.weibo.word',binary=False)
-     if needSave:     
-        model.save('word2vec_model')
-     text_vec = [[model[word] for word in text_cut if word in model] for text_cut in texts_cut]
+def text2vec(texts_cut=None, model_word2vec=None,
+              word2vec_savepath=None, word2vec_loadpath=None,
+              sg=1, size=128, window=5, min_count=1, merge=True):
+     '''
+     文本的词语序列转为词向量序列，可以用于机器学习或者深度学习
+     :param texts_cut: Word list of texts
+     :param model_word2vec: word2vec model of gensim
+     :param word2vec_savepath: word2vec savrpath
+     :param word2vec_loadpath: word2vec loadpath
+     :param sg: 0 CBOW,1 skip-gram
+     :param size: The dimensionality of the feature vectors
+     :param window: The maximum distance between the current and predicted word within a sentence
+     :param min_count: Ignore all words with total frequency lower than this
+     :param merge: If Ture, calculate sentence average vector
+     :return:
+     '''
+     if word2vec_savepath:
+         model_word2vec.save(word2vec_savepath)
+     text_vec = [[model_word2vec[word] for word in text_cut if word in model_word2vec] for text_cut in texts_cut]
      if merge:
          return np.array([sum(i) / len(i) for i in text_vec])
      else:
@@ -61,11 +73,11 @@ for i in range(0,len(X_cut_n)):
 del X_cut_n
 del y
 
-# texts to word vector
 
-word2vec_loadpath='text.model.bin'
-#model_word2vec = word2vec.Word2Vec(X_cut, sg=1, size=128, window=5, min_count=1)
-x_word_vec = text2vec(texts_cut=X_cut,word2vec_loadpath=word2vec_loadpath)
+new_model=word2vec.load(r'C:\Users\liu01\Desktop\sgns.weibo.word')
+# texts to word vector、
+model_word2vec = word2vec.Word2Vec(X_cut, sg=1, size=128, window=5, min_count=1)
+x_word_vec = text2vec(texts_cut=X_cut,model_word2vec=model_word2vec, sg=1, size=20, window=5, min_count=1)
 # texts vector
 #x_vec = np.array([sum(i) / len(i) for i in x_word_vec])
 X_train, X_test, y_train, y_test = train_test_split(x_word_vec, label, test_size=0.2,random_state=1)
